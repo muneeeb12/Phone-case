747130c71f1590d4746dfaf37422925f8acb6800
muneeeb12 <itsmuneebkhan@gmailcom>
2025-02-21 00:51:04 +0500

    Add project files.

---
+++ Phonecase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phonecase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
