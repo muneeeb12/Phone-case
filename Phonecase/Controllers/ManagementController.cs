6ebc1ddf2108c82b3f304e417a7e92a7a37de465
muhammadhaxcan <k214577@nu.edu.pk>
2025-02-21 14:46:57 +0500

    changed product model and make management controller for crud for model and manufacturer

---
+++ Phonecase/Controllers/ManagementController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Phonecase.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    public class ManagementController : Controller {
+        // In-memory lists for demo purposes (replace with database in production)
+        private static List<string> CaseCompanies = new List<string>();
+        private static List<string> PhoneModels = new List<string>();
+
+        // Action to display the management page
+        public IActionResult Index() {
+            ViewBag.CaseCompanies = CaseCompanies;
+            ViewBag.PhoneModels = PhoneModels;
+            return View();
+        }
+
+        // Action to add a case company
+        [HttpPost]
+        public IActionResult AddCaseCompany(string companyName) {
+            if (!string.IsNullOrEmpty(companyName))
+            {
+                CaseCompanies.Add(companyName);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Action to delete a case company
+        [HttpPost]
+        public IActionResult DeleteCaseCompany(string companyName) {
+            CaseCompanies.Remove(companyName);
+            return RedirectToAction("Index");
+        }
+
+        // Action to add a phone model
+        [HttpPost]
+        public IActionResult AddPhoneModel(string modelName) {
+            if (!string.IsNullOrEmpty(modelName)) {
+                PhoneModels.Add(modelName);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Phonecase/Models/Product.cs
+++ Phonecase/Models/Product.cs
@@ -7,16 +7,13 @@ namespace Phonecase.Models
         [Key]
         public int ProductId { get; set; }
 
-        [Required]
-        public string Type { get; set; }  // ENUM Alternative in EF Core
-
         public int? ModelId { get; set; }
         public Model Model { get; set; }  // Navigation Property
 
         public int? CaseManufacturerId { get; set; }
         public CaseManufacturer CaseManufacturer { get; set; }  // Navigation Property
 
-        public string Details { get; set; }
+        public string CaseName { get; set; }
 
         // Navigation Property
         public ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
